5dade9b31958ff9b8662303f4d9aa4d068540b11
nyxeet <nyxeet@ukr.net>
2020-11-07 16:53:14 +0200

    govnocod

---
+++ js/task2.js
@@ -0,0 +1,16 @@
+const countProps = function (obj) {
+    let total = 0;
+    for (const key in obj) {
+        total += 1;
+    }
+    return total;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log("Количество элементов:" , countProps({})); // 0
+
+console.log("Количество элементов:" , countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log("Количество элементов:" , countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
